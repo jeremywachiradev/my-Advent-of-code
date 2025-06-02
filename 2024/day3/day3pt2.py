70f118f293831cffa5fc57ec1b924a658e9d940d
Your Name <you@example.com>
2025-06-02 15:33:05 +0300

    wrong part 2 day 3 but I dont get ht eproblem

--- 2024/day3/day3pt2.py
+++ 2024/day3/day3pt2.py
@@ -8,8 +8,37 @@ def solution(input):
 
     l,r=0,0
     output=0
+    enabled=True
+
     while l<inputlen:
-        if input[l:l+4] == "mul(":
+        if input.startswith("don't()", l):
+            print("one dont")
+            enabled = False
+            l += 7
+            continue
+        if input.startswith("do()", l):
+            print("one do")
+            enabled = True
+            l += 4
+            continue
+        # if input[l:l+len("don't()")] == "don't()":
+        # if input.startswith("don't()", l):
+
+
+        # # if input[l:l+7]=="don't()":
+        #     l+=7
+        #     print("one dont")
+        #     enabled=False
+        #     continue
+        # if input.startswith("do()", l):
+
+        # # if input[l:l+4]=="do()":
+        #     print("one do")
+        #     enabled=True
+        #     l+=4
+        #     continue
+        
+        if input[l:l+4] == "mul(" and enabled:
             bool_val, closing_bracket = isvalidmul(input, l + 3)
             if bool_val:
                 multiplied_value = parse_and_multiplymul(input, l + 3, closing_bracket)
@@ -17,6 +46,7 @@ def solution(input):
                 l = closing_bracket + 1  # move past ")"
                 continue
         l += 1
+        
     return output
 def parse_and_multiplymul(input,opening_bracket,closing_bracket):
     temp_arr=input[opening_bracket+1:closing_bracket].split(",")
